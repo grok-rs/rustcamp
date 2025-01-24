3b4d7648274be27e7e6c2af5932b999205f219cd
serhii-kaliuzhnyi-dev <kalyuzhni.sergei@gmail.com>
2025-01-25 00:04:30 +0200

    2_4

--- 2_idioms/2_3_bound_impl/src/main.rs
+++ 2_idioms/2_3_bound_impl/src/main.rs
@@ -4,7 +4,31 @@ use std::{
 };
 
 fn main() {
-    println!("Refactor me!");
+    // Simple example usage:
+    println!("--- Example usage of the refactored code ---");
+
+    // Create a new empty hydrated aggregate (version=Initial, snapshot=None).
+    let mut hydrated = HydratedAggregate::<MyAggregate>::default();
+    println!("HydratedAggregate before events: {:?}", hydrated);
+
+    // Apply a couple events.
+    hydrated.apply(MyEvent::Increment(10));
+    hydrated.apply(MyEvent::Increment(5));
+
+    println!("After events, version: {:?}", hydrated.version());
+    println!("After events, state: {:?}", hydrated.state());
+
+    // Create an identifier and wrap the hydrated aggregate into an Entity.
+    let my_id = MyAggregateId(String::from("user-123"));
+    let entity = Entity::new(my_id, hydrated);
+
+    // Because we have an impl block for `Entity<I, A>` with
+    //   where I: AggregateId<A>, A: Aggregate
+    // we can call `identifier_str`.
+    println!("Entity version = {:?}", entity.aggregate().version());
+
+    // End of example
+    println!("-------------------------------------------");
 }
 
 /// A projected state built from a series of events.
@@ -24,10 +48,7 @@ pub trait Aggregate: Default {
 }
 
 /// An identifier for an aggregate.
-pub trait AggregateId<A>
-where
-    A: Aggregate,
-{
+pub trait AggregateId<A> {
     /// Gets the stringified aggregate identifier.
     fn as_str(&self) -> &str;
 }
@@ -39,7 +60,7 @@ pub trait Event {
 }
 
 /// An event that can be applied to an aggregate.
-pub trait AggregateEvent<A: Aggregate>: Event {
+pub trait AggregateEvent<A>: Event {
     /// Consumes the event, applying its effects to the aggregate.
     fn apply_to(self, aggregate: &mut A);
 }
@@ -49,12 +70,7 @@ pub trait AggregateEvent<A: Aggregate>: Event {
 pub struct EventNumber(NonZeroU64);
 
 impl EventNumber {
-    /// The minimum [EventNumber].
-    #[allow(unsafe_code)]
-    pub const MIN_VALUE: EventNumber =
-        // One is absolutely non-zero, and this is required for this to be
-        // usable in a `const` context.
-        EventNumber(unsafe { NonZeroU64::new_unchecked(1) });
+    pub const MIN_VALUE: EventNumber = EventNumber(unsafe { NonZeroU64::new_unchecked(1) });
 
     /// Increments the event number to the next value.
     #[inline]
@@ -80,10 +96,6 @@ impl Default for Version {
 }
 
 impl Version {
-    /// Creates a new `Version` from a number.
-    ///
-    /// The number `0` gets interpreted as being `Version::Initial`, while any other number is interpreted as the
-    /// latest event number applied.
     #[inline]
     pub fn new(number: u64) -> Self {
         NonZeroU64::new(number)
@@ -92,7 +104,6 @@ impl Version {
             .unwrap_or(Version::Initial)
     }
 
-    /// Increments the version number to the next in sequence.
     #[inline]
     pub fn incr(&mut self) {
         match *self {
@@ -102,21 +113,24 @@ impl Version {
     }
 }
 
-/// An aggregate that has been loaded from a source, which keeps track of the version of its last snapshot and the current version of the aggregate.
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
-pub struct HydratedAggregate<A>
-where
-    A: Aggregate,
-{
+#[derive(Debug)]
+pub struct HydratedAggregate<A> {
     version: Version,
     snapshot_version: Option<Version>,
     state: A,
 }
 
-impl<A> HydratedAggregate<A>
-where
-    A: Aggregate,
-{
+impl<A: Default> Default for HydratedAggregate<A> {
+    fn default() -> Self {
+        HydratedAggregate {
+            version: Version::Initial,
+            snapshot_version: None,
+            state: A::default(),
+        }
+    }
+}
+
+impl<A> HydratedAggregate<A> {
     /// The current version of the aggregate.
     pub fn version(&self) -> Version {
         self.version
@@ -138,53 +152,47 @@ where
     }
 
     /// Applies a sequence of events to the internal aggregate.
-    pub fn apply_events<E: AggregateEvent<A>, I: IntoIterator<Item = E>>(&mut self, events: I) {
+    pub fn apply_events<E, I>(&mut self, events: I)
+    where
+        A: Aggregate,
+        E: AggregateEvent<A>,
+        I: IntoIterator<Item = E>,
+    {
         for event in events {
             self.apply(event);
         }
     }
 
     /// Applies a single event to the aggregate, keeping track of the new aggregate version.
-    pub fn apply<E: AggregateEvent<A>>(&mut self, event: E) {
+    pub fn apply<E>(&mut self, event: E)
+    where
+        A: Aggregate,
+        E: AggregateEvent<A>,
+    {
         self.state.apply(event);
         self.version.incr();
     }
 }
 
-impl<A> AsRef<A> for HydratedAggregate<A>
-where
-    A: Aggregate,
-{
+impl<A> AsRef<A> for HydratedAggregate<A> {
     fn as_ref(&self) -> &A {
         &self.state
     }
 }
 
-impl<A> Borrow<A> for HydratedAggregate<A>
-where
-    A: Aggregate,
-{
+impl<A> Borrow<A> for HydratedAggregate<A> {
     fn borrow(&self) -> &A {
         &self.state
     }
 }
 
 /// An identified, specific instance of a hydrated aggregate.
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
-pub struct Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+pub struct Entity<I, A> {
     id: I,
     aggregate: HydratedAggregate<A>,
 }
 
-impl<I, A> Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+impl<I, A> Entity<I, A> {
     /// Creates a new entity from an identifier and an associated hydrated aggregate.
     pub fn new(id: I, aggregate: HydratedAggregate<A>) -> Self {
         Entity { id, aggregate }
@@ -206,62 +214,160 @@ where
     }
 }
 
-impl<I, A> From<Entity<I, A>> for HydratedAggregate<A>
+impl<I, A> Entity<I, A>
 where
-    A: Aggregate,
     I: AggregateId<A>,
+    A: Aggregate,
 {
+    pub fn identifier_str(&self) -> &str {
+        self.id.as_str()
+    }
+}
+
+impl<I, A> From<Entity<I, A>> for HydratedAggregate<A> {
     fn from(entity: Entity<I, A>) -> Self {
         entity.aggregate
     }
 }
 
-impl<I, A> AsRef<HydratedAggregate<A>> for Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+impl<I, A> AsRef<HydratedAggregate<A>> for Entity<I, A> {
     fn as_ref(&self) -> &HydratedAggregate<A> {
         &self.aggregate
     }
 }
 
-impl<I, A> AsMut<HydratedAggregate<A>> for Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+impl<I, A> AsMut<HydratedAggregate<A>> for Entity<I, A> {
     fn as_mut(&mut self) -> &mut HydratedAggregate<A> {
         &mut self.aggregate
     }
 }
 
-impl<I, A> Borrow<HydratedAggregate<A>> for Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+impl<I, A> Borrow<HydratedAggregate<A>> for Entity<I, A> {
     fn borrow(&self) -> &HydratedAggregate<A> {
         &self.aggregate
     }
 }
 
-impl<I, A> Borrow<A> for Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
+impl<I, A> BorrowMut<HydratedAggregate<A>> for Entity<I, A> {
+    fn borrow_mut(&mut self) -> &mut HydratedAggregate<A> {
+        &mut self.aggregate
+    }
+}
+
+// We can also let `Entity<I, A>` borrow as `A` if we like:
+impl<I, A> Borrow<A> for Entity<I, A> {
     fn borrow(&self) -> &A {
         self.aggregate.borrow()
     }
 }
 
-impl<I, A> BorrowMut<HydratedAggregate<A>> for Entity<I, A>
-where
-    A: Aggregate,
-    I: AggregateId<A>,
-{
-    fn borrow_mut(&mut self) -> &mut HydratedAggregate<A> {
-        &mut self.aggregate
+/// A concrete type that implements `Aggregate`.
+#[derive(Debug, Default)]
+pub struct MyAggregate {
+    pub count: i64,
+}
+
+impl Aggregate for MyAggregate {
+    fn aggregate_type() -> &'static str {
+        "MyAggregate"
+    }
+}
+
+/// A simple `Event` type for demonstration.
+#[derive(Debug)]
+pub enum MyEvent {
+    Increment(i64),
+    Decrement(i64),
+}
+
+impl Event for MyEvent {
+    fn event_type(&self) -> &'static str {
+        match *self {
+            MyEvent::Increment(_) => "Increment",
+            MyEvent::Decrement(_) => "Decrement",
+        }
+    }
+}
+
+/// Applying our `MyEvent` to `MyAggregate`.
+impl AggregateEvent<MyAggregate> for MyEvent {
+    fn apply_to(self, aggregate: &mut MyAggregate) {
+        match self {
+            MyEvent::Increment(x) => {
+                aggregate.count += x;
+            }
+            MyEvent::Decrement(x) => {
+                aggregate.count -= x;
+            }
+        }
+    }
+}
+
+/// Example of an ID type for `MyAggregate`.
+#[derive(Debug)]
+pub struct MyAggregateId(pub String);
+
+impl AggregateId<MyAggregate> for MyAggregateId {
+    fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_version_increments() {
+        let mut agg = HydratedAggregate::<MyAggregate>::default();
+        assert_eq!(agg.version(), Version::Initial);
+
+        // Apply single event
+        agg.apply(MyEvent::Increment(10));
+        match agg.version() {
+            Version::Number(n) => assert_eq!(n.0.get(), 1),
+            _ => panic!("Expected version to be Number(1) after first event"),
+        }
+
+        // Apply multiple events
+        agg.apply_events(vec![MyEvent::Increment(5), MyEvent::Decrement(3)]);
+        match agg.version() {
+            Version::Number(n) => assert_eq!(n.0.get(), 3),
+            _ => panic!("Expected version to be Number(3) after three events"),
+        }
+    }
+
+    #[test]
+    fn test_aggregate_state_changes() {
+        let mut agg = HydratedAggregate::<MyAggregate>::default();
+        assert_eq!(agg.state().count, 0);
+
+        agg.apply(MyEvent::Increment(10));
+        assert_eq!(agg.state().count, 10);
+
+        agg.apply_events(vec![MyEvent::Increment(5), MyEvent::Decrement(2)]);
+        assert_eq!(agg.state().count, 13);
+    }
+
+    #[test]
+    fn test_entity_id() {
+        let id = MyAggregateId("abc123".to_owned());
+        let agg = HydratedAggregate::<MyAggregate>::default();
+        let entity = Entity::new(id, agg);
+        assert_eq!(entity.identifier_str(), "abc123");
+    }
+
+    #[test]
+    fn test_entity_versions() {
+        let id = MyAggregateId("xyz".to_owned());
+        let mut entity = Entity::new(id, HydratedAggregate::<MyAggregate>::default());
+
+        assert_eq!(entity.aggregate().version(), Version::Initial);
+        entity.aggregate_mut().apply(MyEvent::Increment(2));
+
+        match entity.aggregate().version() {
+            Version::Number(n) => assert_eq!(n.0.get(), 1),
+            _ => panic!("Expected version to be Number(1)"),
+        }
     }
 }
--- 2_idioms/2_4_generic_in_type_out/src/main.rs
+++ 2_idioms/2_4_generic_in_type_out/src/main.rs
@@ -1,10 +1,15 @@
+use derive_more::{AsMut, AsRef};
 use std::net::{IpAddr, SocketAddr};
 
 fn main() {
     println!("Refactor me!");
 
-    let mut err = Error::new("NO_USER".to_string());
-    err.status(404).message("User not found".to_string());
+    let mut err = Error::new("NO_USER");
+    err.status(404).message("User not found");
+    println!("{:?}", err);
+
+    let err2 = Error::default();
+    println!("{:?}", err2);
 }
 
 #[derive(Debug)]
@@ -26,10 +31,16 @@ impl Default for Error {
 }
 
 impl Error {
-    pub fn new(code: String) -> Self {
-        let mut err = Self::default();
-        err.code = code;
-        err
+    pub fn new<S: Into<String>>(code: S) -> Self {
+        Self {
+            code: code.into(),
+            ..Default::default()
+        }
+    }
+
+    pub fn set_status(&mut self, s: u16) -> &mut Self {
+        self.status = s;
+        self
     }
 
     pub fn status(&mut self, s: u16) -> &mut Self {
@@ -37,18 +48,28 @@ impl Error {
         self
     }
 
-    pub fn message(&mut self, m: String) -> &mut Self {
-        self.message = m;
+    pub fn message<S: Into<String>>(&mut self, m: S) -> &mut Self {
+        self.message = m.into();
+        self
+    }
+
+    pub fn set_message<S: Into<String>>(&mut self, m: S) -> &mut Self {
+        self.message = m.into();
         self
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, AsMut, AsRef)]
 pub struct Server(Option<SocketAddr>);
 
 impl Server {
-    pub fn bind(&mut self, ip: IpAddr, port: u16) {
-        self.0 = Some(SocketAddr::new(ip, port))
+    pub fn bind<A: Into<IpAddr>>(&mut self, ip: A, port: u16) -> &mut Self {
+        self.0 = Some(SocketAddr::new(ip.into(), port));
+        self
+    }
+
+    pub fn get_address(&self) -> Option<&SocketAddr> {
+        self.0.as_ref()
     }
 }
 
@@ -65,11 +86,37 @@ mod server_spec {
         fn sets_provided_address_to_server() {
             let mut server = Server::default();
 
-            server.bind(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
-            assert_eq!(format!("{}", server.0.unwrap()), "127.0.0.1:8080");
+            server.bind(Ipv4Addr::new(127, 0, 0, 1), 8080);
+            assert_eq!(
+                format!("{}", server.get_address().unwrap()),
+                "127.0.0.1:8080"
+            );
 
-            server.bind("::1".parse().unwrap(), 9911);
-            assert_eq!(format!("{}", server.0.unwrap()), "[::1]:9911");
+            server.bind("::1".parse::<IpAddr>().unwrap(), 9911);
+            assert_eq!(format!("{}", server.get_address().unwrap()), "[::1]:9911");
         }
+    }
+}
+
+#[cfg(test)]
+mod error_spec {
+    use super::*;
+
+    #[test]
+    fn creates_error_with_default_values() {
+        let err = Error::default();
+        assert_eq!(err.code, "UNKNOWN");
+        assert_eq!(err.status, 500);
+        assert_eq!(err.message, "Unknown error has happened.");
+    }
+
+    #[test]
+    fn allows_chaining_methods_to_customize_error() {
+        let mut err = Error::new("INVALID_INPUT");
+        err.set_status(400).set_message("Invalid input provided");
+
+        assert_eq!(err.code, "INVALID_INPUT");
+        assert_eq!(err.status, 400);
+        assert_eq!(err.message, "Invalid input provided");
     }
 }
